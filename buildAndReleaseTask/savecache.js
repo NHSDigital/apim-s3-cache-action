8f1490ec14413f76c18655cdf0ed01836d03e933
Alex Carrie <acarrie.dev@gmail.com>
2021-01-20 14:27:56 +0000

    APM-1741 Add postbuild task

---
+++ buildAndReleaseTask/savecache.js
@@ -0,0 +1,17 @@
+tl = require('azure-pipelines-task-lib/task');
+
+async function run() {
+    try {
+        const inputString = tl.getInput('samplestring', true);
+        if (inputString == 'bad') {
+            tl.setResult(tl.TaskResult.Failed, 'Bad input was given');
+            return;
+        }
+        console.log('Simulating: Saving -', inputString);
+    }
+    catch (err) {
+        tl.setResult(tl.TaskResult.Failed, err.message);
+    }
+}
+
+run();
