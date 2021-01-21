e010f4f61dfdc09a08006bf2781df7f1abfffb3f
Alex Carrie <acarrie.dev@gmail.com>
2021-01-21 11:01:11 +0000

    APM-1741 Use Node10

--- buildAndReleaseTask/savecache.js
+++ buildAndReleaseTask/savecache.js
@@ -1,9 +1,8 @@
 const tl = require('azure-pipelines-task-lib/task');
-const axios = require('axios');
 
 async function run() {
     try {
-        await axios.get('https://internal-dev.api.service.nhs.uk/canary-api/_ping');
+        console.log('Simulate: Saving Cache', inputString);
     }
     catch (err) {
         tl.setResult(tl.TaskResult.Failed, err.message);
