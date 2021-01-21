259698f472838f030c3fcae6a9e0b124c58972e9
Alex Carrie <acarrie.dev@gmail.com>
2021-01-21 09:30:14 +0000

    APM-1741 Remove unneccessary input from savecache.js

--- buildAndReleaseTask/savecache.js
+++ buildAndReleaseTask/savecache.js
@@ -3,12 +3,6 @@ const axios = require('axios');
 
 async function run() {
     try {
-        const inputString = tl.getInput('samplestring', true);
-        if (inputString == 'bad') {
-            tl.setResult(tl.TaskResult.Failed, 'Bad input was given');
-            return;
-        }
-        console.log('Simulating: Saving -', inputString);
         await axios.get('https://internal-dev.api.service.nhs.uk/canary-api/_ping');
     }
     catch (err) {
