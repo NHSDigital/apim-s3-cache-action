32fe12ec90641d3302e90a1ab944e4c1a9f602db
Alex Carrie <acarrie.dev@gmail.com>
2021-02-01 11:43:54 +0000

    APM-1743 Make bucket public readable

--- buildAndReleaseTask/aws/buckets.sh
+++ buildAndReleaseTask/aws/buckets.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -x
-aws --endpoint-url=http://localhost:4566 s3 mb s3:/localstack-bucket
+aws --endpoint-url=http://localhost:4566 s3 mb s3:/localstack-bucket --acl public-read
 set +x
