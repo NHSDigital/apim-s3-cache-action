7dd7bc0afd39d334b26b257d42606d07f1add321
Alex Carrie <acarrie.dev@gmail.com>
2021-02-16 11:18:44 +0000

    APM-1744 Extract hash part func and has full key

--- s3CacheTask/s3Utils/createCacheKey.js
+++ s3CacheTask/s3Utils/createCacheKey.js
@@ -1,19 +1,35 @@
 const fs = require('fs');
 const path = require('path');
-const md5File = require('md5-file');
+const crypto = require('crypto');
+
+const hashPartIfPath = (part, workingDir) => {
+    const isPath = [part, path.resolve(workingDir, part)].find(p => fs.existsSync(p));
+
+    if (isPath) {
+        let formatPart = part;
+        if (fs.statSync(isPath).isFile()) {
+            const hash = crypto.createHash('sha256');
+            hash.update(isPath);
+            formatPart = hash.digest('hex');
+        }
+        return formatPart;
+    } else {
+        return part;
+    };
+};
+
 
 const createCacheKey = (key, workingDir) => {
-    const keyParts = key.split('|').map((part) => part.trim());
+    const keyParts = key.split('|').map(part => part.trim());
 
-    const keyPartsHashed = keyParts.map((part) => {
-        const path = [part, path.resolve(workingDir, part)].find(p => fs.existsSync(p));
-        if (path) {
-            const formatPart = fs.statSync(path).isFile() ? md5File.sync(path) : part;
-            return formatPart;
-        } else {
-            return part;
-        }
-    })
+    const keyPartsHashed = keyParts.map(part => hashPartIfPath(part, workingDir));
 
-    return keyPartsHashed.join('|');
+    const joinedkeyParts = keyPartsHashed.join('|');
+    const hashedKey = crypto.createHash('sha256').update(joinedkeyParts).digest("hex");
+
+    return hashedKey;
 };
+
+console.log(
+    createCacheKey(`"test data" | s3CacheTask/s3Utils/tests/testData | test.json`,
+    path.resolve(__dirname, 'tests/testData')));
