8fbd8411fe42c313b6c41ebc574dcd90050e57d2
Alex Carrie <acarrie.dev@gmail.com>
2021-02-23 17:01:03 +0000

    APM-1744 Test for successful file extraction

--- s3CacheTask/s3Utils/extractCacheEntry.js
+++ s3CacheTask/s3Utils/extractCacheEntry.js
@@ -2,9 +2,9 @@ const tar = require('tar-stream');
 const fs = require('fs');
 const path = require('path');
 
-const extractCacheEntry = (targetPath, keyName, cacheData) => {
+const extractCacheEntry = (destination, keyName, cacheData) => {
     // Reconfigure to use temp dir
-    fs.writeFileSync(path.resolve(targetPath, `${keyName}.tar`), cacheData);
+    fs.writeFileSync(path.resolve(destination, `${keyName}.tar`), cacheData);
 };
 
 module.exports = extractCacheEntry
---
+++ s3CacheTask/s3Utils/tests/extractCacheEntry.test.js
@@ -0,0 +1,46 @@
+const mockFs = require('mock-fs');
+const path = require('path');
+const AWS = require('aws-sdk');
+const fs = require('fs');
+const extractCacheEntry = require('../extractCacheEntry');
+const retrieveCacheEntry = require('../retrieveCacheEntry');
+const createCacheEntry = require('../createCacheEntry');
+const { createCacheKey } = require('../createCacheKey');
+
+process.env.AWS_ENV = 'localstack';
+
+const credentials = {
+    accessKeyId: 'test-id',
+    secretAccessKey: 'test-secret',
+};
+const bucketName = 'test-extract-bucket';
+
+describe('extractCacheEntry', () => {
+    beforeAll(async () => {
+        const endpoint = 'http://localhost:4566';
+        const s3client = new AWS.S3({
+            credentials,
+            endpoint,
+            s3ForcePathStyle: true
+         });
+
+        await s3client.createBucket({Bucket: bucketName}).promise();
+    });
+
+    test('successfully extracts file from cache buffer', async () => {
+        const targetPath = path.resolve(__dirname, 'testData/test.json');
+        const keyName = await createCacheKey('"test" | testData | testData/test.json', __dirname);
+        await createCacheEntry(targetPath, credentials, bucketName, keyName);
+        const { Body } = await retrieveCacheEntry(keyName, bucketName, credentials);
+
+        // mockFs({
+        //     'cacheFiles': {/** empty directory */}
+        // });
+
+        extractCacheEntry(path.resolve(__dirname, 'cacheFiles'), keyName, Body);
+
+        expect(fs.existsSync(path.resolve(__dirname, 'cacheFiles', `${keyName}.tar`))).toBe(true);
+
+        // mockFs.restore();
+    });
+});
