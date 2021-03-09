bacc23a92ec9c65da24b987c76bb2bab51ba268d
matt.mercer <matt.mercer@nhs.net>
2021-03-09 17:47:37 +0000

    mm: use the package-lock.json

--- s3CacheTask/uploadCache.js
+++ s3CacheTask/uploadCache.js
@@ -3,10 +3,15 @@ const { uploadCache } = require('./s3Utils/taskUtils');
 
 const run = async () => {
     try {
+        const bucket = tl.getInput('bucket', false) || process.env.PIPELINE_CACHE_BUCKET || '';
+        if (bucket === '') {
+            tl.setResult(tl.TaskResult.Failed, `bucket input or $PIPELINE_CACHE_BUCKET required`);
+            return
+        }
         const inputs = {
             key: tl.getInput('key', true),
             location: tl.getInput('location', true),
-            bucket: tl.getInput('bucket', true),
+            bucket: bucket,
             pipelineIsolated: tl.getInput('pipelineIsolated', false)
         };
         console.log('running uploadCache');
