285603c03aa8e1c7c05b46ed486344caf83e2754
saptarshimandal1 <saptarshi.mandal1@nhs.net>
2025-10-07 21:02:57 +0100

    rewrites activate.csh

--- s3CacheTask/s3Utils/tests/s3CacheAction.test.js
+++ s3CacheTask/s3Utils/tests/s3CacheAction.test.js
@@ -348,14 +348,16 @@ describe('S3CacheAction', () => {
             const originalData = fs.readFileSync(`${vars.virtualEnv}/bin/activate.csh`, {encoding: 'utf-8'});
 
             //expect(originalData).toContain('VIRTUAL_ENV "/home/zaphod/apm/apim-s3-cache-action/.venv"');
-            expect(originalData).toContain(`VIRTUAL_ENV="${vars.virtualEnv}"`);
+            //expect(originalData).toContain(`VIRTUAL_ENV="${vars.virtualEnv}"`);
+            expect(originalData).toContain(`setenv VIRTUAL_ENV "${vars.virtualEnv}"`);
 
             await cacheAction.maybeFixPythonVenv(vars.virtualEnv);
 
             const newExec = `exec' ${vars.virtualEnv}/bin/python`;
             const newData = fs.readFileSync(`${vars.virtualEnv}/bin/activate.csh`, {encoding: 'utf-8'});
 
-            expect(newData).toContain(`VIRTUAL_ENV "${vars.virtualEnv}"`);
+            //expect(newData).toContain(`VIRTUAL_ENV "${vars.virtualEnv}"`);
+            expect(newData).toContain(`setenv VIRTUAL_ENV "${vars.virtualEnv}"`);
         })
 
 
